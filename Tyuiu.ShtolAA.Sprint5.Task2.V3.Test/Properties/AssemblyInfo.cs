b03f7c74a9038ea38ff68d9b727bfd9e64f0fe66
aleksandrastol8@gmail.com <aleksandrastol8@gmail.com>
2023-11-19 20:02:22 +0500

    Task2

---
+++ Tyuiu.ShtolAA.Sprint5.Task2.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShtolAA.Sprint5.Task2.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShtolAA.Sprint5.Task2.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a2a7c3e-fe8e-49c3-86e4-69aeeff7b259")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShtolAA.Sprint5.Task2.V3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShtolAA.Sprint5.Task2.V3.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint5.Task2.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { {5,9,1},
+                                         {1,3,9},
+                                         {1,2,2} };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+     
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Штоль А.А. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Штоль Александра Алексеевна | ИИПб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая заменяет нечетные элементы целочисленного   *");
+            Console.WriteLine("* массива 3 на 3 элементов, заполненного значениями с клавиатуры.         *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+     
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Фaйл: " + res);
+            Console.WriteLine("Coздан!");
+            Console.ReadKey();
+        }
+    }
+}
