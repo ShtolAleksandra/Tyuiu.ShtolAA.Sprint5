91f076e5a845bc8be8ac65147ed6ac262f849412
aleksandrastol8@gmail.com <aleksandrastol8@gmail.com>
2023-11-19 23:07:50 +0500

    Task3

---
+++ Tyuiu.ShtolAA.Sprint5.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+namespace Tyuiu.ShtolAA.Sprint5.Task3.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\aleks\source\repos\Tyuiu.ShtolAA.Sprint5\Tyuiu.ShtolAA.Sprint5.Task3.V28\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ShtolAA.Sprint5.Task3.V28/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShtolAA.Sprint5.Task3.V28.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint5.Task3.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Штоль А.А. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Штоль Александра Алексеевна | ИИПб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение выражения при x = 3.     *");
+            Console.WriteLine("* Результат сохранить в бинарный файл OutPutFileTask3.bin и вывести       *");
+            Console.WriteLine("* на консоль.                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Фaйл: " + res);
+            Console.WriteLine("Coздан!");
+            Console.ReadKey();
+        }
+    }
+}
